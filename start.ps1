ecebe56c39cddc623920c9ff39eafb8c835bd3b5
bjlantianyou <bjlantianyou@corp.netease.com>
2017-09-27 18:14:37 +0800

    更新包结构，提供多线程支持。

--- start.ps1
+++ start.ps1
@@ -1,5 +1,6 @@
-$interval = Read-Host "每隔多少秒执行一次测试"
 $scriptPath = $MyInvocation.MyCommand.Path
 $rootPath = Split-Path -Parent $MyInvocation.InvocationName
 $driver = [io.path]::combine($rootPath,"lib","cron.py")
-python $driver $interval 
+$interval = Read-Host "每隔多少秒执行一次测试"
+$multi = Read-Host "设置并发进程数"
+python $driver $interval $multi
